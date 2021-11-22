1bf25753684b8ad060ad8d0980d618639a314d77
Michael Rommel <michamaxe@gmail.com>
2021-11-22 13:51:22 +0100

    fix dependency

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["CustomToolTip"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/chipjarred/SwizzleHelper.git", from: "1.0.0")
+        .package(url: "https://github.com/mrommel/SwizzleHelper.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
